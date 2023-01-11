48ffe72ed2d7882a4ea7bfa3b4070fe64171acee
Ana Delic <milojevican@gmail.com>
2023-01-11 12:24:18 +0100

    Change index.js

--- index.js
+++ index.js
@@ -2,16 +2,18 @@ import chalk from 'chalk';
 import randomColor from 'randomcolor';
 
 const randomHex = randomColor({
-  luminosity: process.argv[2],
+  luminosity: process.argv[3],
   hue: process.argv[2],
 });
 
 console.log(
-  chalk.hex(randomHex)(
-    `##########################
-  ##########################
-  ###### ${randomHex} ######
-  ##########################
-  ##########################`,
-  ),
+  chalk.hex(randomHex)(`##############################
+##############################
+##############################
+########              ########
+########   ${randomHex}    ########
+########              ########
+##############################
+##############################
+##############################`),
 );
